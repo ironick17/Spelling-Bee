21cd93b63dff324c3fd4ef036fa84fe9104f6932
Nick Gall <nick.gall@gmail.com>
2020-05-18 02:00:18 -0400

    Added save to cloud functionality.

--- scripts/index.js
+++ scripts/index.js
@@ -63,6 +63,12 @@ var popupLongMsgMultiplier;
 // ?????
 var gameStartModal;
 
+// ?????
+var gameState;
+
+// ?????
+var timestamp;
+
 /**
  * This section of functions are all invovled in getting information from the NY Times Spelling Bee and setting up this local game.
  * Since the fetch function is asynchronous (returns a promise), all but the last of these functions must be promises as well.
@@ -89,7 +95,8 @@ var gameStartModal;
 // Get the Spelling Bee webpage.
 // fetch returns a promise wrapping a JS response object.
 // @@TODO: Add error handling in case webpage is not available.
-function getSpellingBee() {
+
+async function getSpellingBee() {
   return fetch(
     'https://cors-anywhere.herokuapp.com/https://www.nytimes.com/puzzles/spelling-bee'
     // The webpage comes back as a JS response object, so covert it before returning it.
@@ -105,32 +112,17 @@ function getSpellingBee() {
 // The the string representing the webpage (HTML source) into  DOM object so we can call HTML/DOM methods on it.
 // It returns a prom
 function parseSpellingBee(htmlText) {
-  return new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
     if (!htmlText.includes('<!DOCTYPE html>')) {
-      alert('Error parsing NY Times data.');
+      alert(
+        'Error parsing NY Times data. Using old NY Times data for debugging.'
+      );
       debugMode = true;
       htmlText = nytimesHTMLText;
     }
     let parser = new DOMParser();
     resolve(parser.parseFromString(htmlText, 'text/html'));
   });
-}
-
-function getCloudData() {
-  return fetch('https://jsonbin.io/?????', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'image/jpeg',
-    },
-    mode: 'cors',
-    cache: 'default',
-  })
-    .then((response) => response.json())
-    .catch((reason) => {
-      alert(`Error getting JSONbin.io data: ${reason}`);
-      debugMode = true;
-      return {};
-    });
 }
 
 function getGameInfo(htmlTree) {
@@ -204,10 +196,6 @@ function selectLetter() {
 
 function deleteHandler() {
   wordGuessField.value = wordGuessField.value.slice(0, -1);
-}
-
-function saveHandler() {
-  saveGameClient();
 }
 
 function submitHandler() {
@@ -334,7 +322,7 @@ function getRank(score) {
 }
 
 function saveGameClient() {
-  let gameState = {
+  gameState = {
     gameAnswers,
     gamePangrams,
     maxScore,
@@ -346,38 +334,71 @@ function saveGameClient() {
     gameRank,
     gameWeekday,
     gameDate,
+    // Setting timestamp twice, once local and once cloud. Fix. ?????
     timestamp: Date.now(),
   };
   localStorage.gameState = JSON.stringify(gameState);
 }
 
-function saveGameCloud() {
-  sameGameClient();
-  let gameState = {
-    gameAnswers,
-    gamePangrams,
-    maxScore,
-    gameLevels,
-    outerLetters,
-    centerLetter,
-    answerList,
-    gameScore,
-    gameRank,
-    gameWeekday,
-    gameDate,
-    timestamp: Date.now(),
-  };
+function updateCloudData(cloudData) {
   let saveName = prompt('Who is saving this game?', 'anonymous') || 'anonymous';
-  return fetch('foo')
-    .then()
+  if (cloudData == {}) {
+    alert('Cloud data not found. Reinitializing.');
+    cloudData = {};
+  }
+  if (!cloudData.hasOwnProperty('spellingBee')) {
+    alert('Cloud data not found. Reinitializing.');
+    cloudData.spellingBee = {};
+  }
+  if (!cloudData.spellingBee.hasOwnProperty(saveName)) {
+    cloudData.spellingBee[saveName] = {};
+  }
+  cloudData.spellingBee[saveName].gameState = gameState;
+  return cloudData;
+}
+
+async function setCloudData(cloudData) {
+  let cloudDataString = JSON.stringify(cloudData);
+  return fetch('https://api.jsonbin.io/b/5ebd62b88284f36af7bb1d7b', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'secret-key':
+        '$2b$10$oenHkCkZSs3jpqzXIrl1LeITtaIoYXUBb96HvsRv5vC3Jf4cIVqvy',
+    },
+    body: cloudDataString,
+  })
+    .then(async (response) => {
+      let responseJSON = response.json();
+      return responseJSON.then((response) => response.success);
+    })
     .catch((reason) => {
       alert(`Error storing JSONbin.io data: ${reason}`);
       debugMode = true;
-      return;
+      // need this to be a promise. Fix. ?????
+      return false;
     });
 }
 
+function saveHandler() {
+  saveGameCloud().then((response) =>
+    displayModal(
+      response
+        ? 'Game saved!'
+        : 'Game saved locally, but failed to save to cloud.'
+    )
+  );
+}
+
+async function saveGameCloud() {
+  saveGameClient();
+  return getCloudData().then(updateCloudData).then(setCloudData);
+}
+
 function reloadGameClient() {
+  let gameStateLocal = localStorage.gameState;
+  if (gameStateLocal == undefined) return {};
+  gameStateLocal = JSON.parse(gameStateLocal);
   ({
     gameAnswers,
     gamePangrams,
@@ -391,25 +412,60 @@ function reloadGameClient() {
     gameWeekday,
     gameDate,
     timestamp,
-  } = JSON.parse(localStorage.gameState));
+  } = gameStateLocal);
+  return gameStateLocal;
+}
+
+function getCloudData() {
+  return fetch('https://api.jsonbin.io/b/5ebd62b88284f36af7bb1d7b/latest', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'secret-key':
+        '$2b$10$oenHkCkZSs3jpqzXIrl1LeITtaIoYXUBb96HvsRv5vC3Jf4cIVqvy',
+    },
+  })
+    .then((response) => response.json())
+    .catch((reason) => {
+      alert(`Error getting JSONbin.io data: ${reason}`);
+      debugMode = true;
+      // this catch returns a promise whose resolution value is {}
+      return {};
+    });
 }
 
 function reloadGameCloud(cloudData) {
-  let spellingBeeData = cloudData.spellingBee;
-  let gameState = spellingBeeData.gameState;
-  let gameStateDate = new Date(gameState.gameDate);
-  if (gameStateDate != new Date().setHours(0, 0, 0, 0)) {
+  let saveName = prompt('Who saved this game?', 'anonymous') || 'anonymous';
+  let gameState = reloadGameClient();
+  let spellingBeeData = cloudData.spellingBee || {};
+  let gameStateCloud = (spellingBeeData[saveName] || {}).gameState;
+  if (gameStateCloud == undefined) {
+    if (gameState == {}) {
+      return false;
+    }
     alert(
-      "Game state saved to cloud is not for today's puzzle.\nUsing local saved state instead."
+      'No Spelling Bee Game State stored in cloud.\nUsing local saved state instead.'
     );
-    return;
+    return true;
   }
-  if (timestamp > gameState.timestamp) {
-    confirm(
+  let gameStateDateCloud = new Date(gameStateCloud.gameDate);
+  let gameStateDateLocal = new Date(gameDate);
+  if (gameStateDateCloud.getTime() < gameStateDateLocal.getTime()) {
+    // The cloud data is for a previous day, so use local data.
+    return true;
+  }
+  // Cloud state could be older than the state on this device if the player initially selected New Gam
+  // on this device, then realized they had already played on another device and had gotten further.
+  // So they refreshed the game page and selected Reload Game to get the other device's state stored
+  // in the cloud.
+  if (
+    timestamp &&
+    timestamp > gameStateCloud.timestamp &&
+    !confirm(
       'Game state saved to cloud is older than local saved state.\nUse cloud saved state?'
-    );
-    return;
-  }
+    )
+  )
+    return true;
   ({
     gameAnswers,
     gamePangrams,
@@ -423,16 +479,26 @@ function reloadGameCloud(cloudData) {
     gameWeekday,
     gameDate,
     timestamp,
-  } = gameState);
+  } = gameStateCloud);
+  gameState = gameStateCloud;
+  return true;
 }
 
 function gameStart(startAction) {
   debugMode = false;
   switch (startAction) {
     case 'Reload Game':
-      reloadGameClient();
-      //      getCloudData().then((cloudData) => {reloadGameCloud(); initGameHooks();});
-      initGameHooks();
+      getCloudData().then((cloudData) => {
+        if (reloadGameCloud(cloudData)) {
+          initGameHooks();
+        } else {
+          alert('No cloud or local saved state.\nStarting new game.');
+          getSpellingBee()
+            .then(parseSpellingBee)
+            .then(getGameInfo)
+            .then(setupGame);
+        }
+      });
       break;
     case 'New Game':
       getSpellingBee().then(parseSpellingBee).then(getGameInfo).then(setupGame);
